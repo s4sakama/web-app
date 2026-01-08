faa2301e89ed78392b1f54d84a91d4fb876c0046
s4sakama <botamochijo@gmail.com>
2026-01-08 20:18:50 +0900

    id取得方法の変更

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./style.css";
 
-createRoot(document.querySelector("#content")).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
